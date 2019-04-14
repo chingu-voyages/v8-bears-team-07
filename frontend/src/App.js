2aa46df786c529c140134fc5f5c3e2f92f4a86fd
Paul Rapulu <rapulu@yahoo.com>
2019-04-14 03:51:19 +0100

    landing page navbar and search form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+              WIIYF
+            </a>
+          </nav>
+          <div className="searchForm">
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Type ingredients"
+            />
+            <button type="button" class="btn btn-secondary">
+              Search
+            </button>
+          </div>
         </header>
       </div>
     );
